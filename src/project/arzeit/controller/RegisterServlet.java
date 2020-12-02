f44a647b727131f823556279c40f7aa6786abb59
Minoru Makino <makino111217@gmail,com>
2020-12-02 13:04:58 +0900

    Add a mehotds for POST

--- src/project/arzeit/controller/RegisterServlet.java
+++ src/project/arzeit/controller/RegisterServlet.java
@@ -13,9 +13,9 @@ import project.arzeit.model.AuthCModel;
 import project.arzeit.model.User;
 
 /**
- * project/arzeit/login.htmlに対応するサーブレット
- * ログインボタン押すとここに飛ぶ
- * 認証の結果を返したり 成功したらメインページに飛んだりする
+ * project/arzeit/register.htmlに対応するサーブレット
+ * 登録押すとここに飛ぶ
+ * 登録の結果を返す　0以外だったら失敗
  * @author Minoru Makino
  */
 @WebServlet("/project/arzeit/register")
@@ -24,6 +24,20 @@ public class RegisterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+        
+        ServletContext context = this.getServletContext();
+        DBController db = new DBController( (DataSource)context.getAttribute("dataSource") );
+        AuthCModel authC = new AuthCModel(db);
+
+        int code = authC.register(request.getParameter("id"), request.getParameter("pass")); //登録してエラーコード返すだけ
+
+        StringBuilder json = new StringBuilder("{ \"code\": \""); //コードをjsonデ返す
+        json.append(code).append("\" }");
+
+        response.setContentType("application/json");
+        PrintWriter writer = response.getWriter();
+        writer.append(json.toString());
+        writer.flush();
 
 	}
 }
