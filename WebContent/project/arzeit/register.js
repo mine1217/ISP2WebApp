d1316b6dbc2e701d91dac3c5808c6c937b277abd
recor181 <1810370891y@kindai.ac.jp>
2020-12-04 00:26:40 +0900

    Merge conflict

---
+++ WebContent/project/arzeit/register.js
@@ -0,0 +1,75 @@
+var xmlHttpRequest;
+var idElements;
+var pass1Elements;
+var pass2Elements;
+var nameElements;
+var submitElement;
+​
+​
+function register(){
+​
+  if(6<=idElements.value.length<=24&&idElements.value.match(/^[A-Za-z_0-9]+$/)){
+​
+    if(pass1Elements.value===pass2Elements.value){
+​
+      if(6<=pass1Elements.value.length<=24&&pass1Elements.value.match(/^[A-Za-z_0-9]+$/)){
+        document.getElementById("errormessage").innerHTML = "";
+      }
+      else {
+
+        //エラー文表示
+        document.getElementById("errormessage").innerHTML = "passwordのフォーマットが合っていません";
+        console.log("passwordのフォーマットが合っていません");
+
+      }
+​
+    }else {
+      document.getElementById("errormessage").innerHTML = "passwordが一致しません";
+      console.log("passwordが一致しません");
+    }
+  }
+  else {
+    //エラー文表示
+    document.getElementById("errormessage").innerHTML = "idのフォーマットが合っていません";
+    console.log("idのフォーマットが合っていません");
+  }
+  sendWithPostMethod();
+​
+}
+​
+function sendWithPostMethod(){
+  //passを暗号化
+//     var CryptoJS =  require('crypto-js');
+// ​
+//     var pwd =  "erHt4Mb8s";
+// ​
+//     var encryptedPass = CryptoJS.AES.encrypt(pass1Elements,pwd).toString();
+// ​
+//     var url = "echo";
+//     xmlHttpRequest = new XMLHttpRequest();
+//     xmlHttpRequest.onreadystatechange = receive;
+//     xmlHttpRequest.open("POST",url,true);
+//     xmlHttpRequest.setRequestHeader("Content-Type",
+//     "application/x-www-form-urlencoded");
+//     xmlHttpRequest.send("id=" + idElements.value + "&pass=" + encryptedPass
+//       + "&name=" + nameElements.value);
+
+var url = "echo";
+xmlHttpRequest = new XMLHttpRequest();
+xmlHttpRequest.onreadystatechange = receive;
+xmlHttpRequest.open("POST",url,true);
+xmlHttpRequest.setRequestHeader("Content-Type",
+"application/x-www-form-urlencoded");
+xmlHttpRequest.send("id=" + idElements.value + "&pass=" + pass1Elements
+  + "&name=" + nameElements.value);
+
+}
+​
+window.addEventListener("load", function() {
+  idElements = document.getElementById("id");
+  pass1Elements = document.getElementById("pass1");
+  pass2Elements = document.getElementById("pass2");
+  nameElements = document.getElementById("name");
+  submitElement = document.getElementById("submit")
+	submitElement.addEventListener("click", register, false);
+}, false);
