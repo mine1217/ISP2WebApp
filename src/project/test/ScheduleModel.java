2754ae86ee538258d10b0b4a207d87ba6978f75e
Minoru Makino <makino111217@gmail,com>
2020-12-03 09:41:53 +0900

    Add new file

---
+++ src/project/test/ScheduleModel.java
@@ -0,0 +1,19 @@
+package project.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+import project.arzeit.database.DBController;
+import project.arzeit.database.DataSource;
+import project.arzeit.model.AuthCModel;
+
+/**
+ * AuthCModel-認証処理をするプログラム　の単体テスト
+ * 
+ * @author Minoru Makino
+ */
+public class ScheduleModel {
+    @Test
+
+}
